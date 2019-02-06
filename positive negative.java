f25e638df3185f7eecec071074c894e110405994
parkavi12 <47377605+parkavi12@users.noreply.github.com>
2019-02-06 13:32:13 +0530

    Update positive negative.java

--- positive negative.java
+++ positive negative.java
@@ -1,25 +1,24 @@
  import java.util.Scanner;
-public class positive_negative
-{
- public static void main(String[] args)
+ public class java
  {
-  int n;
-  Scanner s=new Scanner(System.in);
-  System.out.println("Input:");
-  n=s.nextInt();
-  System.out.println(n);
-  System.out.println("Output:");
-  if(n>0)
-  {
-   System.out.println("positive");
-  }
-  else if(n<0)
-  {
-   System.out.println("negative");
-  }
-  else
-  {
-   System.out.println("invalid");
-  }
+     public static void main(String[] args)
+     {
+         int number=4;
+         if(number>0)
+         {
+             if(number%2==0)
+             
+             {
+                 System.out.println("even");
+         }
+         else
+         {
+             System.out.println("odd");
+         }
+     }
+     else
+     {
+         System.out.println("invalid input");     
+     }
  }
-}
+ }
