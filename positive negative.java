e982e1881f95ab19402390bd7273ce6fa2eb30dd
parkavi12 <47377605+parkavi12@users.noreply.github.com>
2019-02-06 11:58:01 +0530

    Update positive negative.java

--- positive negative.java
+++ positive negative.java
@@ -1,19 +1,25 @@
- class PositiveNegative {
-
-    public static void main(String[] args) {
-
-        double number = 12.3;
-
-        // true if number is less than 0
-        if (number < 0.0)
-            System.out.println(number + " is a negative number.");
-
-        // true if number is greater than 0
-        else if ( number > 0.0)
-            System.out.println(number + " is a positive number.");
-
-        // if both test expression is evaluated to false
-        else
-            System.out.println(number + " is 0.");
-    }
+ import java.util.Scanner;
+public class positive_negative
+{
+ public static void main(String[] args)
+ {
+  int n;
+  Scanner s=new Scanner(System.in);
+  System.out.println("Input:");
+  n=s.nextInt();
+  System.out.println(n);
+  System.out.println("Output:");
+  if(n>0)
+  {
+   System.out.println("positive");
+  }
+  else if(n<0)
+  {
+   System.out.println("negative");
+  }
+  else
+  {
+   System.out.println("invalid");
+  }
+ }
 }
