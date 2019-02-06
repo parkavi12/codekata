87e530327897ce9ab3a5a4e5ea3e43a2d0632409
parkavi12 <47377605+parkavi12@users.noreply.github.com>
2019-02-06 14:38:27 +0530

    Update vowelconsonant.java

--- vowelconsonant.java
+++ vowelconsonant.java
@@ -2,7 +2,7 @@ public class VowelConsonant {
 
     public static void main(String[] args) {
 
-        char ch = 'i';
+        char ch = 'p';
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
