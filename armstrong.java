b87d07301b97d48eb44b5506294faccab3d15a6b
parkavi12 <47377605+parkavi12@users.noreply.github.com>
2019-05-23 18:31:00 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class armstrong
+{  
+    public static void main(String args[])
+    { 
+        Scanner input =new Scanner(System.in);
+        int n=input.nextInt();
+        int c=0,a,temp;  
+    
+    temp=n;  
+    while(n>0)  
+    {  
+    a=n%10;  
+    n=n/10;  
+    c=c+(a*a*a);  
+    }  
+    if(temp==c)  
+    System.out.println("yes");   
+    else  
+        System.out.println("no"); 
+    }  
+} 
