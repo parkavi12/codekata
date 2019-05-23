4192ca4dff18a43ab05f8c8cc3a9692da331f165
parkavi12 <47377605+parkavi12@users.noreply.github.com>
2019-05-23 17:51:29 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,16 @@
+ 
+import java.util.*;
+ public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input=new Scanner(System.in);
+	    int num1=input.nextInt();
+	    int num2=input.nextInt();
+	    int power=1; 
+            for(int i=0;i<num2;i++)
+            {
+                power=power*num1;
+            }
+	        System.out.print(power);
+	    }
+}
