3580d00d9d0d46c2e8342b39318385bcbd1dc15e
parkavi12 <47377605+parkavi12@users.noreply.github.com>
2019-05-23 18:20:05 +0530

    Create EvenIntervel.java

---
+++ EvenIntervel.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main
+{  
+    public static void main(String args[])
+    { 
+        Scanner input =new Scanner(System.in);
+        int s=input.nextInt();
+        int e=input.nextInt();
+        List<Integer>result=new ArrayList<Integer>();
+        for(int i=s+1;i<e;i++)
+        {
+            if(i%2==0)
+            {
+               result.add(i);
+            }
+        }
+        for(int i=0;i<result.size();i++)
+        {
+            System.out.print(result.get(i));
+            if(i<result.size()-1)
+            {
+                System.out.print(" ");
+            }
+        }
+    }  
+} 
