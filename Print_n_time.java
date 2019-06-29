c87765b30d0a1d94cfade79e16a3dfa5ffcc6d09
parkavi12 <47377605+parkavi12@users.noreply.github.com>
2019-06-29 17:30:19 +0530

    Create Print_n_time.java

---
+++ Print_n_time.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Print_n_time
+{
+	public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		String str=input.next();
+		int n=input.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+		System.out.println(str);			
+		}	
+		
+	}
+}
