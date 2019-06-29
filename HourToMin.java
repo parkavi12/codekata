1bc8eddb0b592b5d2d508f8df2dd23167904656f
parkavi12 <47377605+parkavi12@users.noreply.github.com>
2019-06-29 16:21:32 +0530

    Create HourToMin.java

---
+++ HourToMin.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class HourToMin
+{
+	public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+			System.out.println(n/60+" "+n%60);
+		
+	}
+}
