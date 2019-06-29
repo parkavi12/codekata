c77f4cb5310a443814f11782fc3570dbd4febe35
parkavi12 <47377605+parkavi12@users.noreply.github.com>
2019-06-29 16:37:57 +0530

    Create CountDigit.java

---
+++ CountDigit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class CountDigit
+{
+	public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		String sen=input.nextLine();
+		int count=0;
+		for(int i=0;i<sen.length();i++)
+		{
+			if(!Character.isWhitespace(sen.charAt(i)))
+			{
+				count++;
+			}
+		}
+		System.out.println(count);		
+	}
+} 
